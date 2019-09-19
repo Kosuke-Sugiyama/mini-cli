243cf31e1499d401964d09614dc5c79977395d0c
Kosuke <sheep.a08@gmail.com>
2019-09-19 14:41:35 +0900

    divmodを使ったリファクタリング

--- warikan.py
+++ warikan.py
@@ -2,7 +2,8 @@ def main():
     total = int(input('合計金額 : '))
     number_of_people = int(input('人数 : '))
 
-    print(f'一人当たり{total // number_of_people}円です。端数は{total % number_of_people}円です。')
+    payment, amari = divmod(total, number_of_people)
+    print(f'一人当たり{payment}円です。端数は{amari}円です。')
 
 
 if __name__ == '__main__':
